b859902f7afe9f526641c86cc58eb682c3bb1af7
Marco Lacoste <marc-olivier.lacoste@flo.com>
2024-12-04 18:41:55 -0500

    part1

---
+++ day4/main.py
@@ -0,0 +1,50 @@
+def main():
+    with open('input.txt', 'r') as file:
+        lines = file.read().splitlines()
+
+        matrix = [list(l) for l in lines]
+
+        count = 0
+
+        n = len(matrix)
+
+        # horizontal check
+        horizontals = [''.join(row) for row in matrix]
+
+        for h in horizontals:
+            count += h.count('XMAS')
+            count += h[::-1].count('XMAS')
+
+        # vertical check
+        verticals = [''.join([row[i] for row in matrix]) for i in range(len(matrix[0]))]
+
+        for v in verticals:
+            count += v.count('XMAS')
+            count += v[::-1].count('XMAS')
+
+        # diagonal check LR
+        diagonal_lrs = []
+
+        for d in range(-n + 1, n):
+            diagonal_lrs.append(''.join(matrix[i][i + d] for i in range(n) if 0 <= i + d < n))
+
+        for dlrs in diagonal_lrs:
+            count += dlrs.count('XMAS')
+            count += dlrs[::-1].count('XMAS')
+
+        # diagonal check RL
+        diagonal_rls = []
+        
+        for d in range(2 * n - 1):
+            diagonal_rls.append(''.join(matrix[i][d - i] for i in range(n) if 0 <= d - i < n))
+
+        for drls in diagonal_rls:
+            count += drls.count('XMAS')
+            count += drls[::-1].count('XMAS')
+
+        print(count)
+
+
+if __name__ == "__main__":
+    main()
+
